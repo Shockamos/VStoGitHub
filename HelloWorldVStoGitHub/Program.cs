cc3c54e093e3b6ad4edc40592b1fdbe89619e883
Shockamos <shockamos@gmail.com>
2016-03-30 23:10:42 +0100

    Added Text to CW

--- HelloWorldVStoGitHub/Program.cs
+++ HelloWorldVStoGitHub/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorldVStoGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Hello World!");
         }
     }
 }
